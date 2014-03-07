a8a798931787cbdafe72c7848bedcdfaeb459eb7
gggeek <gg@ez.no>
2014-03-07 23:21:50 +0000

    Add info files

---
+++ ezinfo.php
@@ -0,0 +1,16 @@
+<?php
+
+class ezdbintegrityInfo
+{
+    static function info()
+    {
+        return array(
+            'Name' => "<a href=\"http://projects.ez.no/ezdbintegrity\">eZ DB Integrity extension</a>",
+            'Version' => "0.1",
+            'Copyright' => "Copyright (C) 2014 Gaetano Giunta",
+            'License' => "GNU General Public License v2.0"
+        );
+    }
+}
+
+?>
