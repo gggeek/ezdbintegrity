065177ecc3911a7c6c6fe6a8af665ff1714e8624
gggeek <gg@ez.no>
2014-03-11 10:53:14 +0000

    Allow scripts to run without autoload generation

--- bin/php/checkattributes.php
+++ bin/php/checkattributes.php
@@ -11,6 +11,11 @@ require 'autoload.php';
 
 // Inject our own autoloader after the std one, as this script is supposed to be
 // executable even when extension has not been activated
+require_once ( dirname( __FILE__ ) . '/../../classes/ezdbiautoloadhelper.php' );
+spl_autoload_register( array( 'ezdbiAutoloadHelper', 'autoload' ) );
+
+// Inject our own autoloader after the std one, as this script is supposed to be
+// executable even when extension has not been activated
 ////spl_autoload_register( array( 'autoloadHelper', 'autoload' ) );
 
 $cli = eZCLI::instance();
@@ -100,75 +105,3 @@ $cli->output();
 $cli->output( ezdbiReportGenerator::getText( $violations, $checker->getChecks(), $options['displaychecks'] ) );
 
 $script->shutdown();
-
-
-/*
-
-/**
-* manages autoloading for classes contained within this extension
-* /
-class autoloadHelper
-{
-    protected static $ezpClasses = null;
-
-    public static function autoload( $className )
-    {
-        if ( !is_array( self::$ezpClasses ) )
-        {
-            self::initializeAutoload();
-        }
-        if ( isset( self::$ezpClasses[$className] ) )
-        {
-            require( self::$ezpClasses[$className] );
-        }
-    }
-
-    protected static function initializeAutoload()
-    {
-        $autoloadOptions = new ezpAutoloadGeneratorOptions();
-
-        $autoloadOptions->basePath = 'extension/ggsysinfo';
-
-        $autoloadOptions->searchKernelFiles = false;
-        $autoloadOptions->searchKernelOverride = false;
-        $autoloadOptions->searchExtensionFiles = true;
-        $autoloadOptions->searchTestFiles = false;
-        $autoloadOptions->writeFiles = false;
-        $autoloadOptions->displayProgress = false;
-
-        $autoloadGenerator = new eZAutoloadGenerator( $autoloadOptions );
-        // We have to jump through hoops to get eZAutoloadGenerator give us back an array
-        $autoloadGenerator->setOutputCallback( array( 'autoloadHelper', 'autoloadCallback' ) );
-
-        try
-        {
-            $autoloadGenerator->buildAutoloadArrays();
-            $autoloadGenerator->printAutoloadArray();
-        }
-        catch ( Exception $e )
-        {
-            echo $e->getMessage() . "\n";
-        }
-    }
-
-    /**
-     * Used as callback for eZAutoloadGenerator
-     * /
-    public static function autoloadCallback( $php, $label )
-    {
-        // callback is called many times with info messages, only use the good one
-        if ( strpos( $php, '<?php' ) !== 0 )
-        {
-            return;
-        }
-        $php = str_replace( array( '<?php', '?>', ), '', $php );
-        self::$ezpClasses = eval( $php );
-        // fix path to be proper relative to eZ root
-        foreach ( self::$ezpClasses as $key => $val )
-        {
-            self::$ezpClasses[$key] = 'extension/ggsysinfo/' . $val;
-        }
-    }
-}
-
-*/
