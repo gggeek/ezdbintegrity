91cade5e44833307ff060b7505ba5366fd9f95e4
gggeek <giunta.gaetano@gmail.com>
2016-05-19 19:22:57 +0100

    Welcome 2016

--- ezinfo.php
+++ ezinfo.php
@@ -7,7 +7,7 @@ class ezdbintegrityInfo
         return array(
             'Name' => "<a href=\"http://projects.ez.no/ezdbintegrity\">eZ DB Integrity extension</a>",
             'Version' => "0.3",
-            'Copyright' => "Copyright (C) 2014 Gaetano Giunta",
+            'Copyright' => "Copyright (C) 2014-2016 Gaetano Giunta",
             'License' => "GNU General Public License v2.0"
         );
     }
