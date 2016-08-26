e4104e1d1332e6219c8dfd5d83fd55d8c9e4ec14
gggeek <giunta.gaetano@gmail.com>
2016-08-26 16:58:09 +0100

    Update version info; add changelogs

--- ezinfo.php
+++ ezinfo.php
@@ -6,7 +6,7 @@ class ezdbintegrityInfo
     {
         return array(
             'Name' => "<a href=\"http://projects.ez.no/ezdbintegrity\">eZ DB Integrity extension</a>",
-            'Version' => "0.8.0",
+            'Version' => "0.8.1",
             'Copyright' => "Copyright (C) 2014-2016 Gaetano Giunta",
             'License' => "GNU General Public License v2.0"
         );
