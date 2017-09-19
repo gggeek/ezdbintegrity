3d2fa84347dfce3314805a85eab2c03f2ce4acf1
gggeek <giunta.gaetano@gmail.com>
2017-09-19 12:21:30 +0100

    add one more custom check

--- ezinfo.php
+++ ezinfo.php
@@ -6,8 +6,8 @@ class ezdbintegrityInfo
     {
         return array(
             'Name' => "<a href=\"http://projects.ez.no/ezdbintegrity\">eZ DB Integrity extension</a>",
-            'Version' => "0.8.1",
-            'Copyright' => "Copyright (C) 2014-2016 Gaetano Giunta",
+            'Version' => "0.10.0",
+            'Copyright' => "Copyright (C) 2014-2017 Gaetano Giunta",
             'License' => "GNU General Public License v2.0"
         );
     }
