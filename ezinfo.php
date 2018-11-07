dd7c3fdc74134173ff74676adee2145721fbde1f
gggeek <giunta.gaetano@gmail.com>
2018-11-07 17:32:22 +0100

    Docs; prepare for release

--- ezinfo.php
+++ ezinfo.php
@@ -6,7 +6,7 @@ class ezdbintegrityInfo
     {
         return array(
             'Name' => "<a href=\"http://projects.ez.no/ezdbintegrity\">eZ DB Integrity extension</a>",
-            'Version' => "0.10.1",
+            'Version' => "0.11.0",
             'Copyright' => "Copyright (C) 2014-2018 Gaetano Giunta",
             'License' => "GNU General Public License v2.0"
         );
