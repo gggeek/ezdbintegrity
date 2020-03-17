f73dab1439000cfd22104c6efecaf156d28f0163
gggeek <giunta.gaetano@gmail.com>
2020-03-17 13:27:41 +0100

    welcome 2020; add checks for invalid values in policy limitations

--- ezinfo.php
+++ ezinfo.php
@@ -6,8 +6,8 @@ class ezdbintegrityInfo
     {
         return array(
             'Name' => "<a href=\"http://projects.ez.no/ezdbintegrity\">eZ DB Integrity extension</a>",
-            'Version' => "0.18.0",
-            'Copyright' => "Copyright (C) 2014-2019 Gaetano Giunta",
+            'Version' => "0.19.0",
+            'Copyright' => "Copyright (C) 2014-2020 Gaetano Giunta",
             'License' => "GNU General Public License v2.0"
         );
     }
