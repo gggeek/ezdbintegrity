9f219fd9b72bb0eccb748c1bb08c50d5ea5017a4
gggeek <giunta.gaetano@gmail.com>
2022-11-03 10:33:00 +0000

    WIP fix tests on php 5.6

--- Tests/phpunit/01_CommandsTest.php
+++ Tests/phpunit/01_CommandsTest.php
@@ -4,35 +4,55 @@ include_once(__DIR__.'/CommandExecutingTest.php');
 
 class CommandsTest extends CommandExecutingTest
 {
+    static $backupArgv;
+
     /**
      * @param string $legacyScript legacy script
-     * @param array $argv used to inject arguments and options into legacy cli scripts
+     * @param array $args used to inject arguments and options into legacy cli scripts
      * @return void
      *
      * @dataProvider provideGenericCommandsList
+     *
+     * @todo with php 7.4, we (sometimes) get Uncaught UnexpectedValueException: RecursiveDirectoryIterator::__construct in ezdbintegrity/vendor/phpunit/php-file-iterator/src/Facade.php on line 36
      */
-    public function testLegacyCommands($legacyScript, $argv = array())
+    public function testLegacyCommands($legacyScript, $args = array())
     {
-        /// @todo in between each test, we should reset to the original $argv...
-        if ($argv) {
-            foreach($argv as $arg)
-                $GLOBALS['argv'][] = $arg;
-        } else {
-            $GLOBALS['argv'][] = basename($legacyScript);
+        if (!is_array(self::$backupArgv)) {
+            self::$backupArgv = $GLOBALS['argv'];
         }
 
+        // This is required to pass in legacy-script arguments
+        $GLOBALS['argv'] = array_merge(self::$backupArgv, array($legacyScript), $args);
+        $_SERVER['argv'] = $GLOBALS['argv'];
+
+        // There is a bug in LegacyEmbedScriptCommand when running many legacy scripts in a row. We hack around it
+        $container = $this->getContainer();
+        // first, we make sure not to try to instantiate the web-kernel-handler
+        $ch = $this->getContainer()->get('ezpublish_legacy.kernel_handler.cli');
+        $container->set( 'ezpublish_legacy.kernel.lazy', null );
+        $container->set( 'ezpublish_legacy.kernel_handler', $ch );
+        $container->set( 'ezpublish_legacy.kernel_handler.web', $ch );
+        // then we get the kernel
+        /** @var callable $k */
+        $k = $container->get('ezpublish_legacy.kernel');
+        $k = $k();
+        // and patch *its own* cli handler
+        /** @var \eZ\Publish\Core\MVC\Legacy\Kernel\CLIHandler $cl */
+        $ch = \Closure::bind(function () { return $this->kernelHandler; }, $k, 'ezpKernel');
+        $ch = $ch();
+        $ch->setEmbeddedScriptPath($legacyScript);
+
+        /// @todo try wrapping this in one more ob_start
         $output = $this->runCommand('ezpublish:legacy:script', array('script' => $legacyScript));
         // CLIHandler does not close its output buffering. We do it, or phpunit will mark the test as risky
-        //var_dump(ob_get_level());
-        //ob_implicit_flush(false);
-        //$output2 = ob_get_contents();
         ob_end_clean();
     }
 
     public function provideGenericCommandsList()
     {
         $out = array(
-            array('extension/ezdbintegrity/bin/php/checkattributes.php', array('checkattributes.php', '*')),
+            array('extension/ezdbintegrity/bin/php/checkattributes.php', array('*')),
+            array('extension/ezdbintegrity/bin/php/checkattributes.php', array('--displaychecks')),
             array('extension/ezdbintegrity/bin/php/checkschema.php', array()),
             array('extension/ezdbintegrity/bin/php/checkstorage.php', array()),
             array('extension/ezdbintegrity/bin/php/generatedefsfrompersistentobjects.php', array()),
